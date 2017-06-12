3b0d6c0c3ea6e61d6029b4769f2bb99902524d2d
MUHAMMAD SALMAN BIN MOHD SHAH <msalman26@live.utm.my>
2017-06-12 23:21:03 +0800

    Schedule updated tapi date tu kiv lagi

--- ExpressBus/Customer/Schedule.aspx.cs
+++ ExpressBus/Customer/Schedule.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ExpressBus.Customer
 {
@@ -11,11 +14,14 @@ namespace ExpressBus.Customer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            Session["origin"] = DropDownList1.SelectedValue.ToString();
+            Session["destination"] = DropDownList2.SelectedValue.ToString();
+            
             Response.Redirect("~/Customer/SelectBus.aspx");
         }
     }
--- ExpressBus/Customer/Schedule.aspx.designer.cs
+++ ExpressBus/Customer/Schedule.aspx.designer.cs
@@ -13,6 +13,33 @@ namespace ExpressBus.Customer {
     public partial class Schedule {
         
         /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// DropDownList2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
+        
+        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
