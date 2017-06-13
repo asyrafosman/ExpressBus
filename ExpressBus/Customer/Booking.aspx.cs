39b96e15d953b4e6ac9a70357c03d209c69823b8
Mohamad Asyraf Osman <mesaasyraf@yahoo.com>
2017-06-14 00:34:32 +0800

    update Booking

--- ExpressBus/Customer/Booking.aspx.cs
+++ ExpressBus/Customer/Booking.aspx.cs
@@ -14,69 +14,65 @@ namespace ExpressBus.Customer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Label1.Text = Session["fname"].ToString();
-            Label9.Text = Session["telno"].ToString();
-            Label10.Text = Session["email"].ToString();
-            Label4.Text = Session["Date"].ToString();
-
-            int busId = Convert.ToInt32(Session["BusId"]);
-
-
-
-
-            string connStr = ConfigurationManager.ConnectionStrings["ExpressBusCS"].ConnectionString;
-            SqlConnection conn = new SqlConnection(connStr);
-            conn.Open();
-
-            SqlCommand cmd = new SqlCommand("MakeBooking", conn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("BusId", busId);
-            
-
-            SqlDataReader drUser;
-            drUser = cmd.ExecuteReader();
-            drUser.Read();
-
-            try
+            if (Session["uname"] == null)
             {
-                if (drUser.HasRows)
+                Response.Redirect("Default.aspx");
+            }
+
+            else
+            {
+                Label1.Text = Session["fname"].ToString();
+                Label9.Text = Session["telno"].ToString();
+                Label10.Text = Session["email"].ToString();
+                Label4.Text = Session["Date"].ToString();
+
+                int busId = Convert.ToInt32(Session["BusId"]);
+
+                string connStr = ConfigurationManager.ConnectionStrings["ExpressBusCS"].ConnectionString;
+                SqlConnection conn = new SqlConnection(connStr);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("MakeBooking", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("BusId", busId);
+
+                SqlDataReader drUser;
+                drUser = cmd.ExecuteReader();
+                drUser.Read();
+
+                try
                 {
-                    Session["origin"] = drUser["Origin"].ToString();
-                    Session["destination"] = drUser["Destination"].ToString();
-                    Session["depart"] = drUser["Depart"].ToString();
-                    Session["arrive"] = drUser["Arrive"].ToString();
-                    Session["busNo"] = drUser["BusNo"].ToString();
-                    Session["duration"] = drUser["Duration"].ToString();
-                    Session["price"] = drUser["Price"].ToString();
-                    
+                    if (drUser.HasRows)
+                    {
+                        Session["origin"] = drUser["Origin"].ToString();
+                        Session["destination"] = drUser["Destination"].ToString();
+                        Session["depart"] = drUser["Depart"].ToString();
+                        Session["arrive"] = drUser["Arrive"].ToString();
+                        Session["busNo"] = drUser["BusNo"].ToString();
+                        Session["duration"] = drUser["Duration"].ToString();
+                        Session["price"] = drUser["Price"].ToString();
+
+                        conn.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string script = "alert('" + ex.Message.ToString() + "'); window.location.reload();\n";
+                    Page.ClientScript.RegisterStartupScript(GetType(), "msgbox", script, true);
+                }
+                finally
+                {
                     conn.Close();
-                    
                 }
-                else
-                {
-                    
-                }
-            }
-            catch (Exception ex)
-            {
-                string script = "alert('" + ex.Message.ToString() + "'); window.location.reload();\n";
-                Page.ClientScript.RegisterStartupScript(GetType(), "msgbox", script, true);
-            }
-            finally
-            {
-                conn.Close();
-            }
 
-            Label2.Text = Session["origin"].ToString();
-            Label3.Text = Session["destination"].ToString();
-            Label5.Text = Session["depart"].ToString();
-            Label6.Text = Session["arrive"].ToString();
-            Label7.Text = Session["duration"].ToString();
-            Label8.Text = Session["price"].ToString();
-            Label11.Text = Session["busNo"].ToString();
-           
-
-
+                Label2.Text = Session["origin"].ToString();
+                Label3.Text = Session["destination"].ToString();
+                Label5.Text = Session["depart"].ToString();
+                Label6.Text = Session["arrive"].ToString();
+                Label7.Text = Session["duration"].ToString();
+                Label8.Text = Session["price"].ToString();
+                Label11.Text = Session["busNo"].ToString();
+            }
         }
     }
 }
