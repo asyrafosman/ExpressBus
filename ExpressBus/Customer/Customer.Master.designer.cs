12f4cc706cbd27a4ec2a2f932224b5a9de46f7cf
naim mansor <naim_mansor95@yahoo.com>
2017-06-13 23:35:44 +0800

    master page

--- ExpressBus/Customer/Customer.Master.designer.cs
+++ ExpressBus/Customer/Customer.Master.designer.cs
@@ -82,7 +82,7 @@ namespace ExpressBus.Customer {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnLogout;
+        protected global::System.Web.UI.WebControls.LinkButton btnLogout;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
