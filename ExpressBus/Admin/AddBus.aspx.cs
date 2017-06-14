07169e070979737e50ab1916676772e8723ffc58
Mohamad Asyraf Osman <mesaasyraf@yahoo.com>
2017-06-14 10:35:10 +0800

    include all changes

--- ExpressBus/Admin/AddBus.aspx.cs
+++ ExpressBus/Admin/AddBus.aspx.cs
@@ -14,7 +14,10 @@ namespace ExpressBus.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["adminuname"] == null)
+            {
+                Response.Redirect("AdminLogin.aspx");
+            }
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
